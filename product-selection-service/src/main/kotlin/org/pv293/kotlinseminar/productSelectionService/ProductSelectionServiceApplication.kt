de3fe725df6660293d95597ae8c7affbf62e7046
Ondřej Hložek <hlozek.o1@gmail.com>
2026-01-24 16:45:32 +0100

    feat: add product-selection service module skeleton

---
+++ product-selection-service/src/main/kotlin/org/pv293/kotlinseminar/productSelectionService/ProductSelectionServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.pv293.kotlinseminar.productSelectionService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductSelectionServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductSelectionServiceApplication>(*args)
+}
