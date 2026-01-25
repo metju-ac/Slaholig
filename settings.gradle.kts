54fa26b8b46765d9cf7261acaa26516988bc1f9e
Matej Klima <matej.klima5@gmail.com>
2026-01-25 23:31:53 +0100

    build: remove unused services from gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,6 @@
 rootProject.name = "kotlin-seminar"
 
 include("shared")
-include("courses-service")
-include("enrollment-service")
-include("forum-service")
-include("assignment-service")
 include("product-selection-service")
 include("payment-service")
 include("product-delivery-service")
